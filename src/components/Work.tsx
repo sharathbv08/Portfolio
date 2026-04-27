18ada7d71d491866ce2fbb294b713714da577fa9
Sharath Bv <sharathbasavakumar@gmail.com>
2026-04-27 17:01:59 +0530

    fix unused imports

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -1,6 +1,5 @@
-import { useState, useCallback } from "react";
+import { useState } from "react";
 import "./styles/Work.css";
-import { MdArrowBack, MdArrowForward } from "react-icons/md";
 
 const projects = [
   {
